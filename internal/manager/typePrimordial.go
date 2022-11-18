60612cfa7f8994b1e891e75bc3f01f7fe89f17e6
Helmut Kemper <helmut.kemper@gmail.com>
2022-11-18 10:11:21 -0300

    initial commit

--- internal/manager/typePrimordial.go
+++ internal/manager/typePrimordial.go
@@ -10,14 +10,14 @@ type Primordial struct {
 //
 // Create a docker network to be used in the chaos test
 //
-//	 Input:
-//	    name: network name
-//			subnet: subnet value. eg. 10.0.0.0/16
-//			gateway: gateway value. eg. "10.0.0.1
+//	Input:
+//	  name: network name
+//	  subnet: subnet value. eg. 10.0.0.0/16
+//	  gateway: gateway value. eg. "10.0.0.1
 //
-//		Notes:
-//	   * If there is already a network with the same name and the same configuration, nothing will be done;
-//		  * If a network with the same name and different configuration already exists, the network will be deleted and a new network created.
+//	Notes:
+//	  * If there is already a network with the same name and the same configuration, nothing will be done;
+//	  * If a network with the same name and different configuration already exists, the network will be deleted and a new network created.
 func (el *Primordial) NetworkCreate(name, subnet, gateway string) (err error) {
 	network := new(Network)
 	network.New(el.Manager)
