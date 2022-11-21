748837acd42611e0053e95de69d20ec111ba3669
Helmut Kemper <helmut.kemper@gmail.com>
2022-11-20 21:07:27 -0300

    working

--- internal/manager/typeContainer_test.go
+++ internal/manager/typeContainer_test.go
@@ -1,14 +1,8 @@
 package manager
 
 import (
-	"context"
 	"github.com/helmutkemper/chaos/internal/standalone"
-	"go.mongodb.org/mongo-driver/bson"
 	"testing"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func TestContainerFromImage_Primordial(t *testing.T) {
@@ -21,7 +15,7 @@ func TestContainerFromImage_Primordial(t *testing.T) {
 
 	standalone.GarbageCollector()
 	t.Cleanup(func() {
-		//standalone.GarbageCollector()
+		standalone.GarbageCollector()
 	})
 
 	mng := &Manager{}
@@ -33,31 +27,10 @@ func TestContainerFromImage_Primordial(t *testing.T) {
 		SaveStatistics("../../").
 		FailFlag("../../bugs", "Multi threading initialized").
 		Ports("tcp", 27017, 27016, 27015, 27014).
-		//Volumes("/data/db", "../../internal/builder/test/data0", "../../internal/builder/test/data1", "../../internal/builder/test/data2").
+		Volumes("/data/db", "../../internal/builder/test/data0", "../../internal/builder/test/data1", "../../internal/builder/test/data2").
 		EnvironmentVar("--host 0.0.0.0").
 		Create("mongo:latest", "delete_mongo", 3).
 		Start()
-
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27016"))
-	if err != nil {
-		panic(err)
-	}
-
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
-	}
-
-	usersCollection := client.Database("testing").Collection("users")
-
-	user := bson.D{{"fullName", "User 1"}, {"age", 30}}
-	// insert the bson object using InsertOne()
-	for {
-		_, err = usersCollection.InsertOne(context.TODO(), user)
-		// check for errors in the insertion
-		if err != nil {
-			panic(err)
-		}
-	}
 
 	done := make(chan struct{})
 	done <- struct{}{}
